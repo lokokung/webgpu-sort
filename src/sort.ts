7947221f01fdc970218e0d54da69c4d9d8b307ef
Loko Kung <lokokung@google.com>
2023-03-31 22:14:26 -0700

    Added more user API and tests!

--- src/sort.ts
+++ src/sort.ts
@@ -1,173 +1,325 @@
 import { makeBufferWithContents } from './webgpu/util/buffer.js';
 import { nextPowerOfTwo } from './webgpu/util/math.js';
 
-/** Computes the number of dispatch calls needed for the given the number of elements to sort and
- *  the workgroup size used for the computation.
+/** Sorter that sorts GPUBuffer in place. Currently can only be used with basic builtin types, i.e.
+ * u32, i32, and f32.
  */
-export function computeNumberOfDispatches(n: number, wgs: number): number {
-  let log2 = (x: number) => {
-    return 31 - Math.clz32(x);
-  }
-  const outerLoops = log2(n) - log2(wgs) - 1;
-  const innerLoops = (outerLoops / 2) * (2 * (log2(wgs) + 1) + outerLoops - 1);
-  return (1 + outerLoops + innerLoops);
+export interface InPlaceSorter {
+  /** Sorts the `n` typed elements in `b` in place. */
+  sort(device: GPUDevice, b: GPUBuffer, n: number): void;
 }
 
-/** In-place sort of buffer of u32s. */
-export function sort(device: GPUDevice, buffer: GPUBuffer, n: number): void {
+export interface IndexSorter {
+  /** Returns a GPUBuffer `indices` such that `b[indices[0..n]]` is a sorted view of `b` */
+  sort(device: GPUDevice, b: GPUBuffer, n: number): GPUBuffer;
+}
+
+export interface WGSLFunction {
+  /** WGSL code for the function. */
+  code: string;
+  /** Entry point for the function. */
+  entryPoint: string;
+}
+
+export type SortMode = 'ascending' | 'descending';
+
+interface BuiltinSortElementTypeInfo {
+  /** Less than function. */
+  lt: WGSLFunction;
+  /** Greater than function. */
+  gt: WGSLFunction;
+}
+
+export interface SortElementType {
+  /** Name of the element type. */
+  type: string;
+  /** Direct comparison function to compare two values of the given type. */
+  comp?: WGSLFunction;
+  /** Distance function used to key the values into f32 space. */
+  dist?: WGSLFunction;
+  /** Struct definition of the element type if it is not a simple built-in. */
+  definition?: string;
+}
+
+/** Per built-in element type info. */
+export type BuiltinSortElementType = 'u32' | 'i32' | 'f32';
+const kBuiltinElementTypes: {
+  readonly [k in BuiltinSortElementType]: BuiltinSortElementTypeInfo;
+} =
+  /* prettier-ignore */ {
+  'u32': {
+    lt: {
+      code: "fn _lt(left: u32, right: u32) -> bool { return left < right; }",
+      entryPoint: "_lt",
+    },
+    gt: {
+      code: "fn _gt(left: u32, right: u32) -> bool { return left > right; }",
+      entryPoint: "_gt",
+    },
+  },
+  'i32': {
+    lt: {
+      code: "fn _lt(left: i32, right: i32) -> bool { return left < right; }",
+      entryPoint: "_lt",
+    },
+    gt: {
+      code: "fn _gt(left: i32, right: i32) -> bool { return left > right; }",
+      entryPoint: "_gt",
+    },
+  },
+  'f32': {
+    lt: {
+      code: "fn _lt(left: f32, right: f32) -> bool { return left < right; }",
+      entryPoint: "_lt",
+    },
+    gt: {
+      code: "fn _gt(left: f32, right: f32) -> bool { return left > right; }",
+      entryPoint: "_gt",
+    },
+  },
+};
+
+export interface InPlaceSorterDescriptor {
+  /** Element type that is expected to be sorted. */
+  elementType: SortElementType;
+}
+
+export function createInPlaceSorter(
+  type: BuiltinSortElementType,
+  mode: SortMode = 'ascending'
+): InPlaceSorter {
+  return {
+    sort(device, b, n) {
+      sortBuiltin(type, mode, device, n, b);
+    },
+  };
+}
+
+function createBuiltinSortShader(
+  type: BuiltinSortElementType,
+  mode: SortMode,
+  wgs: number,
+  n: number,
+  kv_pairs: boolean = true
+): string {
+  const info = kBuiltinElementTypes[type];
+  return `
+  struct Params {
+    h: u32,
+    algorithm: u32
+  }
+
+  @group(0) @binding(0) var<storage, read_write> k: array<${type}>;
+  ${kv_pairs ? '@group(0) @binding(1) var<storage, read_write> v: array<u32>;' : ''}
+  @group(1) @binding(0) var<uniform> params: Params;
+
+  // TODO: Since we use 2 arrays here always, this means that work group size needs to be smaller
+  //       even when we are doing in-place for simple built-in types. We could probably have another
+  //       shader instead, but using this for simplicity for now.
+  var<workgroup> local_k: array<${type}, ${wgs * 2}>;
+  ${kv_pairs ? 'var<workgroup> local_v: array<u32, ${wgs * 2}>;' : ''}
+
+  // Drop-in comparison function for the built-ins.
+  ${mode === 'ascending' ? info.lt.code : info.gt.code}
+
+  fn _compare(left: ${type}, right: ${type}) -> bool {
+    return ${mode === 'ascending' ? info.lt.entryPoint : info.gt.entryPoint}(left, right);
+  }
+
+  fn global_compare_and_swap(idx: vec2u) {
+    if (idx.x >= ${n} || idx.y >= ${n}) {
+      return;
+    }
+    if (_compare(k[idx.y], k[idx.x])) {
+      // Always swap the keys.
+      let tmp_k = k[idx.x];
+      k[idx.x] = k[idx.y];
+      k[idx.y] = tmp_k;
+
+      // Conditionally swap the values as well.
+      ${
+        kv_pairs
+          ? `
+      let tmp_v = v[idx.x];
+      v[idx.x] = v[idx.y];
+      v[idx.y] = tmp_v;
+      `
+          : ''
+      }
+    }
+  }
+
+  fn local_compare_and_swap(idx: vec2u, offset: u32) {
+    if (offset + idx.x >= ${n} || offset + idx.y >= ${n}) {
+      return;
+    }
+    if (_compare(local_k[idx.y], local_k[idx.x])) {
+      // Always swap the keys.
+      let tmp_k = local_k[idx.x];
+      local_k[idx.x] = local_k[idx.y];
+      local_k[idx.y] = tmp_k;
+
+      // Conditionally swap the values as well.
+      ${
+        kv_pairs
+          ? `
+      let tmp_v = local_v[idx.x];
+      local_v[idx.x] = local_v[idx.y];
+      local_v[idx.y] = tmp_v;
+      `
+          : ''
+      }
+    }
+  }
+
+  fn local_init(offset: u32, i: u32) {
+    if (offset + i < ${n}) {
+      local_k[i] = k[offset + i];
+      ${kv_pairs ? `local_v[i] = v[offset + i];` : ''}
+    }
+  }
+
+  fn local_flush(offset: u32, i: u32) {
+    if (offset + i < ${n}) {
+      k[offset + i] = local_k[i];
+      ${kv_pairs ? `v[offset + i] = local_v[i];` : ''}
+    }
+  }
+
+  fn big_flip(t_prime: u32, h: u32) {
+    let half_h = h >> 1;
+    let q = ((2 * t_prime) / h) * h;
+    let x = q + (t_prime % half_h);
+    let y = q + h - (t_prime % half_h) - 1;
+
+    global_compare_and_swap(vec2u(x, y));
+  }
+
+  fn big_disperse(t_prime: u32, h: u32) {
+    let half_h = h >> 1;
+    let q = ((2 * t_prime) / h) * h;
+    let x = q + (t_prime % half_h);
+    let y = x + half_h;
+
+    global_compare_and_swap(vec2u(x, y));
+  }
+
+  fn local_flip(t: u32, h: u32, offset: u32) {
+    workgroupBarrier();
+
+    let half_h = h >> 1;
+    let indices =
+      vec2u(h * ((2 * t) / h)) +
+      vec2u(t % half_h, h - 1 - (t % half_h));
+    local_compare_and_swap(indices, offset);
+  }
+
+  fn local_disperse(t: u32, h: u32, offset: u32) {
+    var hh = h;
+    for (; hh > 1; hh /= 2) {
+      workgroupBarrier();
+
+      let half_h = hh >> 1;
+      let indices =
+        vec2u(hh * ((2 * t) / hh)) +
+        vec2u(t % half_h, half_h + (t % half_h));
+      local_compare_and_swap(indices, offset);
+    }
+  }
+
+  fn local_bms(t: u32, h: u32, offset: u32) {
+    for (var hh : u32 = 2; hh <= h; hh *= 2) {
+      local_flip(t, hh, offset);
+      local_disperse(t, hh / 2, offset);
+    }
+  }
+
+  @compute @workgroup_size(${wgs}) fn main(
+    @builtin(local_invocation_id) local_invocation_id: vec3u,
+    @builtin(global_invocation_id) global_invocation_id: vec3u,
+    @builtin(workgroup_id) workgroup_id: vec3u
+  ) {
+    let t = local_invocation_id.x;
+    let t_prime = global_invocation_id.x;
+    let offset = ${wgs} * 2 * workgroup_id.x;
+
+    // Initialize workgroup local memory.
+    if (params.algorithm <= 1) {
+      local_init(offset, t * 2);
+      local_init(offset, t * 2 + 1);
+    }
+
+    switch params.algorithm {
+      case 0: {
+        local_bms(t, params.h, offset);
+      }
+      case 1: {
+        local_disperse(t, params.h, offset);
+      }
+      case 2: {
+        big_flip(t_prime, params.h);
+      }
+      case 3: {
+        big_disperse(t_prime, params.h);
+      }
+      default: {}
+    }
+
+    // Copy local memory back to the buffer.
+    if (params.algorithm <= 1) {
+      workgroupBarrier();
+      local_flush(offset, t * 2);
+      local_flush(offset, t * 2 + 1);
+    }
+  }
+  `;
+}
+
+function sortBuiltin(
+  type: BuiltinSortElementType,
+  mode: SortMode,
+  device: GPUDevice,
+  n: number,
+  k: GPUBuffer,
+  v?: GPUBuffer
+): void {
   const alignedN = nextPowerOfTwo(n);
-  const workGroupSize = Math.min(device.limits.maxComputeWorkgroupSizeX, alignedN / 2);
+  // Halved for k-v pairs in local memory.
+  const maxWorkgroupSize = v
+    ? device.limits.maxComputeWorkgroupSizeX / 2
+    : device.limits.maxComputeWorkgroupSizeX;
+  const workGroupSize = Math.min(maxWorkgroupSize, alignedN / 2);
   const workGroupCount: number = alignedN / (workGroupSize * 2);
 
   // Create the shader.
   const shader: GPUShaderModule = device.createShaderModule({
-    code: `
-    struct Params {
-      h: u32,
-      algorithm: u32
-    }
-
-    @group(0) @binding(0) var<storage, read_write> value: array<u32>;
-    @group(0) @binding(1) var<uniform> params: Params;
-
-    var<workgroup> local_value: array<u32, ${workGroupSize * 2}>;
-
-    // TODO: Probably need the comparator to be passed in or something later on.
-    fn is_smaller(left: u32, right: u32) -> bool {
-      return left < right;
-    }
-
-    fn global_compare_and_swap(idx: vec2u) {
-      if (idx.x >= ${n} || idx.y >= ${n}) {
-        return;
-      }
-      if (is_smaller(value[idx.y], value[idx.x])) {
-        let tmp = value[idx.x];
-        value[idx.x] = value[idx.y];
-        value[idx.y] = tmp;
-      }
-    }
-
-    fn local_compare_and_swap(idx: vec2u, offset: u32) {
-      if (offset + idx.x >= ${n} || offset + idx.y >= ${n}) {
-        return;
-      }
-      if (is_smaller(local_value[idx.y], local_value[idx.x])) {
-        let tmp = local_value[idx.x];
-        local_value[idx.x] = local_value[idx.y];
-        local_value[idx.y] = tmp;
-      }
-    }
-
-    fn big_flip(t_prime: u32, h: u32) {
-      let half_h = h >> 1;
-      let q = ((2 * t_prime) / h) * h;
-      let x = q + (t_prime % half_h);
-      let y = q + h - (t_prime % half_h) - 1;
-
-      global_compare_and_swap(vec2u(x, y));
-    }
-
-    fn big_disperse(t_prime: u32, h: u32) {
-      let half_h = h >> 1;
-      let q = ((2 * t_prime) / h) * h;
-      let x = q + (t_prime % half_h);
-      let y = x + half_h;
-
-      global_compare_and_swap(vec2u(x, y));
-    }
-
-    fn local_flip(t: u32, h: u32, offset: u32) {
-      workgroupBarrier();
-
-      let half_h = h >> 1;
-      let indices =
-        vec2u(h * ((2 * t) / h)) +
-        vec2u(t % half_h, h - 1 - (t % half_h));
-      local_compare_and_swap(indices, offset);
-    }
-
-    fn local_disperse(t: u32, h: u32, offset: u32) {
-      var hh = h;
-      for (; hh > 1; hh /= 2) {
-        workgroupBarrier();
-
-        let half_h = hh >> 1;
-        let indices =
-          vec2u(hh * ((2 * t) / hh)) +
-          vec2u(t % half_h, half_h + (t % half_h));
-        local_compare_and_swap(indices, offset);
-      }
-    }
-
-    fn local_bms(t: u32, h: u32, offset: u32) {
-      for (var hh : u32 = 2; hh <= h; hh *= 2) {
-        local_flip(t, hh, offset);
-        local_disperse(t, hh / 2, offset);
-      }
-    }
-
-    @compute @workgroup_size(${workGroupSize}) fn main(
-      @builtin(local_invocation_id) local_invocation_id: vec3u,
-      @builtin(global_invocation_id) global_invocation_id: vec3u,
-      @builtin(workgroup_id) workgroup_id: vec3u
-    ) {
-      let t = local_invocation_id.x;
-      let t_prime = global_invocation_id.x;
-      let offset = ${workGroupSize} * 2 * workgroup_id.x;
-
-      // Initialize workgroup local memory.
-      if (params.algorithm <= 1) {
-        if (offset + t * 2 < ${n}) {
-          local_value[t * 2] = value[offset + t * 2];
-        }
-        if (offset + t * 2 + 1 < ${n}) {
-          local_value[t * 2 + 1] = value[offset + t * 2 + 1];
-        }
-      }
-
-      switch params.algorithm {
-        case 0: {
-          local_bms(t, params.h, offset);
-        }
-        case 1: {
-          local_disperse(t, params.h, offset);
-        }
-        case 2: {
-          big_flip(t_prime, params.h);
-        }
-        case 3: {
-          big_disperse(t_prime, params.h);
-        }
-        default: {}
-      }
-
-      // Copy local memory back to the buffer.
-      if (params.algorithm <= 1) {
-        workgroupBarrier();
-        if (offset + t * 2 < ${n}) {
-          value[offset + t * 2] = local_value[t * 2];
-        }
-        if (offset + t * 2 + 1 < ${n}) {
-          value[offset + t * 2 + 1] = local_value[t * 2 + 1];
-        }
-      }
-    }
-    `,
+    code: createBuiltinSortShader(type, mode, workGroupSize, n, !!v),
   });
 
   // Create the compute pipeline needed.
   const pipeline = device.createComputePipeline({
-    layout: "auto",
+    layout: 'auto',
     compute: {
       module: shader,
-      entryPoint: "main",
+      entryPoint: 'main',
     },
   });
 
   const encoder = device.createCommandEncoder();
   const pass = encoder.beginComputePass();
   pass.setPipeline(pipeline);
+
+  const bindGroupKV = device.createBindGroup({
+    layout: pipeline.getBindGroupLayout(0),
+    entries: [
+      {
+        binding: 0,
+        resource: { buffer: k },
+      },
+      ...(v ? [{ binding: 1, resource: { buffer: v } }] : []),
+    ],
+  });
+  pass.setBindGroup(0, bindGroupKV);
 
   const paramBuffers: GPUBuffer[] = [];
   let helper = (h: number, algorithm: number) => {
@@ -178,35 +330,31 @@ export function sort(device: GPUDevice, buffer: GPUBuffer, n: number): void {
     );
     paramBuffers.push(params);
 
-    const bindGroup = device.createBindGroup({
-      layout: pipeline.getBindGroupLayout(0),
+    const bindGroupP = device.createBindGroup({
+      layout: pipeline.getBindGroupLayout(1),
       entries: [
         {
           binding: 0,
-          resource: { buffer },
-        },
-        {
-          binding: 1,
           resource: { buffer: params },
         },
       ],
     });
 
-    pass.setBindGroup(0, bindGroup);
+    pass.setBindGroup(1, bindGroupP);
     pass.dispatchWorkgroups(workGroupCount);
-  }
+  };
   let local_bms = (h: number) => {
     helper(h, 0);
-  }
+  };
   let local_disperse = (h: number) => {
     helper(h, 1);
-  }
+  };
   let big_flip = (h: number) => {
     helper(h, 2);
-  }
+  };
   let big_disperse = (h: number) => {
     helper(h, 3);
-  }
+  };
 
   let h = workGroupSize * 2;
   local_bms(h);
---
+++ src/tests/sort.spec.ts
@@ -0,0 +1,97 @@
+export const description = `
+Basic tests for sorting.
+`;
+
+import { Fixture } from '../common/framework/fixture.js';
+import { makeTestGroup } from '../common/framework/test_group.js';
+import { getGPU } from '../common/util/navigator_gpu.js';
+import { TypedArrayBufferView, assert } from '../common/util/util.js';
+import { BuiltinSortElementType, SortMode, createInPlaceSorter } from '../sort.js';
+import { makeBufferWithContents } from '../webgpu/util/buffer.js';
+
+export const g = makeTestGroup(Fixture);
+
+function generateBuiltinData(type: BuiltinSortElementType, n: number): TypedArrayBufferView {
+  switch (type) {
+    case 'f32': {
+      return new Float32Array(Array.from({ length: n }, () => Math.random()));
+    }
+    case 'i32': {
+      return new Int32Array(
+        Array.from({ length: n }, () => Math.floor(Math.random() * 4294967295) - 2147483648)
+      );
+    }
+    case 'u32': {
+      return new Uint32Array(
+        Array.from({ length: n }, () => Math.floor(Math.random() * 4294967295))
+      );
+    }
+  }
+}
+
+g.test('builtins,inplace')
+  .desc('Tests in-place, key only sorting of builtin types.')
+  .params(u =>
+    u
+      .combine('type', ['u32', 'i32', 'f32'] as BuiltinSortElementType[])
+      .combine('mode', ['ascending', 'descending'] as SortMode[])
+      .beginSubcases()
+      .combine('size', [16, 64, 100, 2048, 10000])
+  )
+  .fn(async t => {
+    // Initialize WebGPU stuff.
+    const gpu = getGPU();
+    const adapter = await gpu.requestAdapter();
+    assert(adapter !== null);
+    const device = await adapter.requestDevice();
+    assert(device !== null);
+
+    // Get the parameters.
+    const { type, mode, size } = t.params;
+
+    const data = generateBuiltinData(type, size);
+    const buffer = makeBufferWithContents(
+      device,
+      data,
+      GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC
+    );
+    await device.queue.onSubmittedWorkDone();
+    data.sort((a, b) => mode === 'ascending' ? a - b : b - a);
+    const sorter = createInPlaceSorter(type, mode);
+    sorter.sort(device, buffer, size);
+    await device.queue.onSubmittedWorkDone();
+
+    // Copy the data to a readable buffer.
+    const sorted = device.createBuffer({
+      size: buffer.size,
+      usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ
+    })
+    const encoder = device.createCommandEncoder()
+    encoder.copyBufferToBuffer(buffer, 0, sorted, 0, buffer.size);
+    device.queue.submit([encoder.finish()]);
+    await device.queue.onSubmittedWorkDone();
+    await sorted.mapAsync(GPUMapMode.READ);
+    let result: TypedArrayBufferView;
+    switch (type) {
+      case 'f32': {
+        result = new Float32Array(sorted.getMappedRange());
+        break;
+      }
+      case 'i32': {
+        result = new Int32Array(sorted.getMappedRange());
+        break;
+      }
+      case 'u32': {
+        result = new Uint32Array(sorted.getMappedRange());
+        break;
+      }
+    }
+
+    // Naive comparison for simplicity for now.
+    for (let i = 0; i < size; i++) {
+      t.expect(data[i] === result[i]);
+    }
+
+    // Destroy the device to free the resources.
+    device.destroy();
+  });
