90ca08bf2ba7ed4c801bc67a5e2770414d443086
Loko Kung <lokokung@google.com>
2023-04-10 20:17:08 -0700

    Update build

--- dist/0.x/sort.module.d.ts
+++ dist/0.x/sort.module.d.ts
@@ -2,7 +2,7 @@
 /** Sort mode type. */
 export type SortMode = 'ascending' | 'descending';
 /** Basic element type interface that is extended by implementations. */
-interface ElementType {
+interface SortElementType {
     /** Name of the element type. */
     type: string;
     /** Struct definition of the element type if applicable. */
@@ -19,28 +19,36 @@ export interface WGSLFunction {
 export interface WGSLDistanceFunction extends WGSLFunction {
     /** Type of the result computed via the distance function. Note that it must be an in-place
      *  sortable element. */
-    distType: SortInPlaceElementType;
+    distType: ComparisonElementType;
+    /** Additional bind groups that may be needed for the distance function. Note that bind group 0
+     *  is reserved and cannot be used here. */
+    bindGroups?: {
+        index: number;
+        bindGroupLayout: GPUBindGroupLayout;
+        bindGroup: GPUBindGroup;
+    }[];
 }
-/** In-place sort element type definition and a set of ease-of-use defaults for common types. */
-export interface SortInPlaceElementType extends ElementType {
+/** In-place sort element type definition. */
+export interface ComparisonElementType extends SortElementType {
     /** Comparison function for the elements, should return true when 'left' < 'right'. */
     comp: WGSLFunction;
 }
-interface SortInPlaceElementTypeMap {
-    u32: SortInPlaceElementType;
-    i32: SortInPlaceElementType;
-    f32: SortInPlaceElementType;
-    vec2u: SortInPlaceElementType;
-    vec3u: SortInPlaceElementType;
-    vec4u: SortInPlaceElementType;
-    vec2i: SortInPlaceElementType;
-    vec3i: SortInPlaceElementType;
-    vec4i: SortInPlaceElementType;
-    vec2f: SortInPlaceElementType;
-    vec3f: SortInPlaceElementType;
-    vec4f: SortInPlaceElementType;
+/** A set of ease-of-use defaults for common types. */
+interface ComparisonElementTypeMap {
+    u32: ComparisonElementType;
+    i32: ComparisonElementType;
+    f32: ComparisonElementType;
+    vec2u: ComparisonElementType;
+    vec3u: ComparisonElementType;
+    vec4u: ComparisonElementType;
+    vec2i: ComparisonElementType;
+    vec3i: ComparisonElementType;
+    vec4i: ComparisonElementType;
+    vec2f: ComparisonElementType;
+    vec3f: ComparisonElementType;
+    vec4f: ComparisonElementType;
 }
-export declare const SortInPlaceElementType: SortInPlaceElementTypeMap;
+export declare const ComparisonElementType: ComparisonElementTypeMap;
 export interface InPlaceSorter {
     /** Encodes the sort into the given encoder in it's own pass(es). */
     encode(encoder: GPUCommandEncoder): void;
@@ -53,7 +61,7 @@ export interface InPlaceSorterConfig {
     /** The GPU device. */
     device: GPUDevice;
     /** The type of data to sort. */
-    type: SortInPlaceElementType;
+    type: ComparisonElementType | DistanceElementType;
     /** The number of elements expected in the buffer. */
     n: number;
     /** The sort mode of the sorter, defaults to ascending if not specified. */
@@ -62,8 +70,8 @@ export interface InPlaceSorterConfig {
     buffer: GPUBuffer;
 }
 /** Index sort element types definition. */
-export interface SortIndexElementType extends ElementType {
-    /** Distance function used to key the values into a in-place sortable type. */
+export interface DistanceElementType extends SortElementType {
+    /** Distance function used to key the values into an in-place sortable type. */
     dist: WGSLDistanceFunction;
 }
 export interface IndexSorter {
@@ -78,24 +86,15 @@ export interface IndexSorterConfig {
     /** The GPU device. */
     device: GPUDevice;
     /** The type of data to sort. */
-    type: SortIndexElementType;
+    type: ComparisonElementType | DistanceElementType;
     /** The number of elements expected in the buffer. */
     n: number;
     /** The sort mode of the sorter, defaults to ascending if not specified. */
     mode?: SortMode;
     /** Holds the raw data. */
     buffer: GPUBuffer;
-    /** Holds the computed distance values. Optional and will be created if not provided. */
-    k?: GPUBuffer;
     /** Holds the sorted indices. Optional and will be created if not provided. */
-    v?: GPUBuffer;
-    /** Additional bind groups that may be needed for the distance function. Note that bind group 0
-     *  is reserved and cannot be used here. */
-    bindGroups?: {
-        index: number;
-        bindGroupLayout: GPUBindGroupLayout;
-        bindGroup: GPUBindGroup;
-    }[];
+    indices?: GPUBuffer;
 }
 export declare function createInPlaceSorter(config: InPlaceSorterConfig): InPlaceSorter;
 export declare function createIndexSorter(config: IndexSorterConfig): IndexSorter;
